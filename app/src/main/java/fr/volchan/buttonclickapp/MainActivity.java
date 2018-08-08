16b236d6e9429167a8faf4380af4dd2c841cb571
stephane de la torre <s.dlt@hotmail.fr>
2018-08-08 19:01:24 +0200

    initial commit

---
+++ app/src/main/java/fr/volchan/buttonclickapp/MainActivity.java
@@ -0,0 +1,37 @@
+package fr.volchan.buttonclickapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        userInput.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = userInput.getText().toString() + "\n";
+                    textView.append(result);
+                    userInput.setText("");
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
+    }
+}
